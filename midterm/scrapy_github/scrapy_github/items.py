aa6ac8cb96ab15aca8374bf831591c87c5821f6b
Muugii1234 <nyamsurenmunkhtselmeg@gmail.com>
2025-04-14 11:22:15 +0800

    Delete midterm/scrapy_github directory

--- midterm/scrapy_github/scrapy_github/items.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-class ScrapyGithubItem(scrapy.Item):
-    url = scrapy.Field()
-    about = scrapy.Field()
-    last_updated = scrapy.Field()
-    languages = scrapy.Field()
-    commits = scrapy.Field()
